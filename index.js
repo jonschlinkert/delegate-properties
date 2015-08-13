ae6af5251b09dd22caf59298a7fe497c4158a612
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-12 22:09:05 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/*!
+ * delegate-methods <https://github.com/jonschlinkert/delegate-methods>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+var defineProp = require('define-property');
+
+/**
+ * Copy properties from `provider` to `receiver` and make them non-enumerable.
+ *
+ * @param  {Object} `receiver`
+ * @param  {Object} `provider`
+ */
+
+module.exports = function delegate(receiver, provider) {
+  for (var key in provider) {
+    defineProp(receiver, key, provider[key]);
+  }
+};
